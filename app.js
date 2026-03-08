54b1ffb5dabec43db01f90b777d9d748028b9024
rabby784islam-lab <rabby784islam@gmail.com>
2026-03-08 08:43:16 +0600

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T02:43:12.786Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-08T02:43:24.913Z
+Commit: Fix broken links in documentation
