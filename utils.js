d929d026b28bfc0025975083d57053932c928654
rabby784islam-lab <rabby784islam@gmail.com>
2026-03-08 08:43:52 +0600

    Fix race condition in async operations

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T02:43:26.154Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-08T02:43:59.985Z
+Commit: Fix race condition in async operations
